a224b23022c7929807a88ec29487e994fed27ecc
yogesh-bhagia <87267789+yogesh-bhagia@users.noreply.github.com>
2021-07-11 19:12:02 +0530

    Update sqoop_script.py

--- Ingestion Framework/sqoop_script.py
+++ Ingestion Framework/sqoop_script.py
@@ -17,9 +17,10 @@ with open('config.json') as cj:
 
 try:
     query='''sqoop import  --connect {c} --username {un} --password {pd} --table {tb} --m 1 --target-dir {td} --hive-import --create-hive-table'''.format(c=val['conn'], 
-                                                                                                                                                        un=val['username'],                                                                                                                                                            pd=val['password'], 
-                                                                                                                                                        tb=val['tabName'], 
-                                                                                                                                                        td=val['tarDir'],)
+                                                                                                                                                          un=val['username'],
+                                                                                                                                                          pd=val['password'], 
+                                                                                                                                                          tb=val['tabName'], 
+                                                                                                                                                          td=val['tarDir'],)
 
     subprocess.run(query, shell=True)
 
