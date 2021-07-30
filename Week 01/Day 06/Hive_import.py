26ef989bb9113d934381344c52ea1a641ec5adea
yogesh-bhagia <87267789+yogesh-bhagia@users.noreply.github.com>
2021-07-30 07:10:32 +0530

    Add files via upload

---
+++ Week 01/Day 06/Hive_import.py
@@ -0,0 +1,28 @@
+
+#Importing All Packages
+#----------------------------
+
+import json
+import subprocess
+
+#Connecting with Config File
+#----------------------------
+
+with open('config.json') as cj:
+    val = json.load(cj)
+
+
+#Exception Handeling  
+#----------------------------
+
+try:
+    query='''sqoop import  --connect {c} --username {un} --password {pd} --table {tb} --m 1 --target-dir {td} --hive-import --create-hive-table'''.format(c=val['conn'], 
+                                                                                                                                                          un=val['username'],
+                                                                                                                                                          pd=val['password'], 
+                                                                                                                                                          tb=val['tabName'], 
+                                                                                                                                                          td=val['tarDir'],)
+
+    subprocess.run(query, shell=True)
+
+except Exception as ex:
+    print(ex)
